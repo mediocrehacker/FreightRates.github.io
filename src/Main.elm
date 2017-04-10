a19ec0dd1de985c13a3ea346c24c8c79f73a4059
mediocreHacker <mediocreHacker@protonmail.com>
2017-04-10 10:58:41 +0200

    Layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,40 @@
 module Main exposing (..)
 
-import Html exposing (Html, program, div)
+import Html exposing (Html, program, div, text, h1, h2, h3, span, p)
+import Html.Attributes exposing (style)
 import Material
+import Material.Scheme as Scheme
+import Material.Color as Color
 import Material.Layout as Layout
+import Material.Toggles as Toggles
+import Material.Button as Button
+import Material.Options as Options
+import Material.Typography as Typo
+import Material.Icon as Icon
+import Material.Grid as Grid
+import Material.Color as Color
+import Material.Elevation as Elevation
+import Material.Typography as Typography
 
 
 -- Model
 
 
 type alias Model =
-    { pol : String
+    { mdl : Material.Model
+    , pol : String
     , pod : String
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { pol = "", pod = "" }, Cmd.none )
+    ( { mdl = Material.model
+      , pol = ""
+      , pod = ""
+      }
+    , Cmd.none
+    )
 
 
 
@@ -24,7 +42,8 @@ init =
 
 
 type Msg
-    = NoOp
+    = Mdl (Material.Msg Msg)
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -33,6 +52,9 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
 
 
 -- View
@@ -40,14 +62,61 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Layout.render Mdl
-        model.mdl
-        [ Layout.fixedHeader ]
-        { header = []
-        , drawer = []
-        , tabs = ( [], [] )
-        , main = []
-        }
+    Scheme.topWithScheme Color.Blue Color.Orange <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader ]
+            { header = header
+            , drawer = drawer
+            , tabs = ( [], [] )
+            , main = [ viewMain model ]
+            }
+
+
+viewMain : Model -> Html Msg
+viewMain model =
+    div
+        [ style
+            [ ( "padding-right", "8%" )
+            , ( "padding-left", "8%" )
+            ]
+        ]
+        [ Options.styled h1
+            [ Color.text Color.primary ]
+            [ text "FreightRates" ]
+        , Options.styled h3
+            [ Typo.headline ]
+            [ text "Search Best Freight Shipping Rates" ]
+        ]
+
+
+header : List (Html Msg)
+header =
+    [ Layout.row []
+        [ Layout.title [] [ text "FreightRates" ]
+        , Layout.spacer
+        , Layout.navigation []
+            [ Layout.link
+                [ Layout.href "#" ]
+                [ span [] [ text "Sign In" ] ]
+            ]
+        ]
+    ]
+
+
+drawer : List (Html Msg)
+drawer =
+    [ Layout.title [] [ text "FreightRates" ]
+    , Layout.navigation
+        []
+        [ Layout.link
+            [ Layout.href "#" ]
+            [ text "About" ]
+        , Layout.link
+            [ Layout.href "#" ]
+            [ text "Benefits" ]
+        ]
+    ]
 
 
 main : Program Never Model Msg
